19b01d2261376a6d52df99dd1fcdb4eca02f015a
Todd Carlson <todd.carlson3030@me.com>
2020-06-29 17:59:30 -0600

    Added delete button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 
-export default function TodoList({todos}) {
+export default function TodoList({todos, deleteTodo}) {
 
-    const showTodos = todos.map(todo => <TodoItem key={todo.id} {...todo} />)
-    
+    const showTodos = todos.map(todo => <TodoItem key={todo.id} deleteTodo={deleteTodo} {...todo} />)
+
     return (
         <ul className='todo-list'>
             {showTodos}
