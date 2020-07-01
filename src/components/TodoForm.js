c05bb2739cbdaac794364d2da4db7d54a1ce5f04
Todd Carlson <todd.carlson3030@me.com>
2020-07-01 16:56:43 -0600

    Got all state changes working properly on controlled form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 
 export default class TodoForm extends Component {
+
+    state = {
+        title: "",
+        content: "",
+        urgent: false
+    } 
+
+    handleChange = (event) => { 
+        const {name} = event.target
+        const value = name === 'urgent' ? event.target.checked : event.target.value
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
         return(
-           <form> 
+           <form className='todo-form'> 
                <h2>Add New Todo</h2>
                <label>Title</label>
-               <input name='title' />
+               <input name='title' value={this.state.title} onChange={this.handleChange} />
                <label>Content</label>
-               <input name='content' />
+               <input name='content' value={this.state.content} onChange={this.handleChange} />
                <label>Urgent</label>
-               <input type='checkbox' name='urgent' />
+               <input type='checkbox' name='urgent' value={this.state.urgent} onChange={this.handleChange} />
                <input type='submit' />
            </form>
         )
