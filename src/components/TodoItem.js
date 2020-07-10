2e7410660b5e3ea9dcefb25ef918ef74e2abdf45
Todd Carlson <todd.carlson3030@me.com>
2020-07-10 17:49:14 -0600

    Added toggle-edit-form hook

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,23 @@
-import React from 'react' 
+import React, { useState } from 'react' 
+import TodoForm from './TodoForm'
 
 export default ({id, title, content, urgent, deleteTodo}) => {
 
+    const [toggleEditForm, setToggleEditForm] = useState(false)
+
     const handleClick = () => {
         deleteTodo(id)
-    }
+    } 
+
+    const showTodo = () => (
+        <li className={urgent ? "todo-item urgent" : "todo-item"}>
+        <h2>{title}</h2>
+        <p>{content}</p>
+        <button className="delete" onClick={handleClick}>DELETE</button>
+    </li>
+    )
 
     return(
-        <li className={urgent ? "todo-item urgent" : "todo-item"}>
-            <h2>{title}</h2>
-            <p>{content}</p>
-            <button className="delete" onClick={handleClick}>DELETE</button>
-        </li>
+       
     )
 } 
